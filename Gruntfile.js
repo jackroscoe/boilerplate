6dd92c29c8d081c3352ebaf55ea7faa1fa7797ef
Jack Roscoe <jack@novni.com>
2013-11-09 11:25:01 +0000

    First commit. Install basic deps + configs.

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    "compass": {
+      "compile": {
+        "options": {
+          "config": 'config.rb'
+        }
+      }
+    },
+    "jshint": {
+      "app": [
+        'js/main.js'
+      ]
+    },
+    "watch": {
+      "compass": {
+        "tasks": "compass:compile",
+        "files": "sass/**.scss"
+      },
+      "jshint": {
+        "tasks": 'jshint:app',
+        "files": 'js/main.js'
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  require('load-grunt-tasks')(grunt);
+
+  // Default task.
+  grunt.registerTask('dev', ['watch']);
+
+
+};
